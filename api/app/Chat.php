3b4036a983f005184db04abf5974bcdf390526d0
claudelkros <desiretaira@gmail.com>
2019-09-13 12:18:40 +0000

    frontend added

--- api/app/Chat.php
+++ api/app/Chat.php
@@ -8,6 +8,8 @@ class Chat extends Model
 {
     //
     protected $fillable = [
-        'sender_id', 'receiver_id','content',
+        'receiver_id',
+        'sender_id',
+        'content'
     ];
 }
