62b984bc2524fca9cd7a270a41fecb3dd691f038
bellatez <gisele.takwi@gmail.com>
2019-09-27 12:33:31 +0100

    update of the information table

--- api/database/migrations/2019_09_23_114639_create_information_table.php
+++ api/database/migrations/2019_09_23_114639_create_information_table.php
@@ -20,6 +20,7 @@ class CreateInformationTable extends Migration
             $table->text('content');
             $table->string('date')->nullable();
             $table->string('media')->nullable();
+            $table->unsignedBigInteger('posted_by')->nullable();
             $table->timestamps();
         });
     }
