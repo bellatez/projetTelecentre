cc6f1b1ef4ce712ee161f88966bce5c1dc70b456
claudelkros <desiretaira@gmail.com>
2019-10-08 16:48:28 +0000

    Utilisation de VueJs pour le Fronend

--- Frontend/Public/src/App.vue
+++ Frontend/Public/src/App.vue
@@ -16,21 +16,21 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+
     </v-content>
+    <router-view></router-view>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+
   },
   data: () => ({
-    //
+
   }),
 };
 </script>
