48d424f9d28ccdc81bdab1fac6bb0f7073a1e8e2
claudelkros <desiretaira@gmail.com>
2019-10-09 08:37:05 +0000

    Vuejs design

--- Frontend/Public/src/App.vue
+++ Frontend/Public/src/App.vue
@@ -6,12 +6,8 @@
         <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
+      <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest">
+        <span class="mr-2">Rechercher</span>
       </v-btn>
     </v-app-bar>
 
